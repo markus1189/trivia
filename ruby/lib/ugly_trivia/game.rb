f2f6be1c2213faa8a34ffb1f1b6f791457cc44a4
Markus Hauck <markus1189@gmail.com>
2012-07-22 16:11:07 +0200

    Moved all print statements to moderator

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -1,20 +1,24 @@
+require_relative "moderators"
 module UglyTrivia
   PlayerNotFoundError = Class.new(StandardError)
+  UnknownCategoryError = Class.new(StandardError)
   class Game
     def  initialize
       @players = []
       @places = Array.new(6, 0)
       @purses = Array.new(6, 0)
       @in_penalty_box = Array.new(6) { false }
-      
+
       @pop_questions = []
       @science_questions = []
       @sports_questions = []
       @rock_questions = []
-      
+
       @current_player_index = 0
       @is_getting_out_of_penalty_box = false
-      
+
+      @moderator = GameModerator.silent
+
       50.times do |i|
         @pop_questions.push "Pop Question #{i}"
         @science_questions.push "Science Question #{i}"
@@ -30,22 +34,20 @@ module UglyTrivia
     def current_player
       @players.fetch(@current_player_index)
     end
-    
+
     def playable?
       how_many_players >= 2
     end
-    
-    def add(player_name)
-      @players.push player_name
+
+    def add(player)
+      @players.push player
       @places[how_many_players] = 0
       @purses[how_many_players] = 0
-      
-      puts "#{player_name} was added"
-      puts "They are player number #{@players.length}"
-      
+
+      @moderator.added_player(player,how_many_players)
       true
     end
-    
+
     def how_many_players
       @players.length
     end
@@ -56,7 +58,7 @@ module UglyTrivia
     end
 
     def in_penalty_box?(p)
-      index = @players.index(p) 
+      index = @players.index(p)
       index or raise PlayerNotFoundError
       @in_penalty_box.fetch(index)
     end
@@ -64,26 +66,25 @@ module UglyTrivia
     def current_player_gets_out?
       @is_getting_out_of_penalty_box
     end
-    
+
     def roll(roll)
-      puts "#{@players[@current_player_index]} is the current player"
-      puts "They have rolled a #{roll}"
-      
+      @moderator.current_player(current_player)
+      @moderator.rolled(current_player,roll)
+
       if in_penalty_box?(current_player)
         if roll % 2 != 0
           @is_getting_out_of_penalty_box = true
 
-          puts "#{@players[@current_player_index]} is getting out of the penalty box"
+          @moderator.player_gets_out_of_penalty_box(current_player)
           @places[@current_player_index] = @places[@current_player_index] + roll
           @places[@current_player_index] = @places[@current_player_index] - 12 if @places[@current_player_index] > 11
-          
-          puts @players[@current_player_index] +
-                '\'s new location is' +
-                @places[@current_player_index].to_s
-          puts "The category is #{current_category}"
+
+          @moderator.moved_player(current_player,
+                                  position_for_player(current_player),
+                                  current_category)
           ask_question
         else
-          puts "#{@players[@current_player_index]} is not getting out of the penalty box"
+          @moderator.player_stays_in_penalty_box(current_player)
           @is_getting_out_of_penalty_box = false
           end
 
@@ -92,23 +93,33 @@ module UglyTrivia
         @places[@current_player_index] = @places[@current_player_index] + roll
         @places[@current_player_index] = @places[@current_player_index] - 12 if @places[@current_player_index] > 11
 
-        puts @players[@current_player_index] +
-              '\'s new location is' +
-              @places[@current_player_index].to_s
-        puts "The category is #{current_category}"
+        @moderator.moved_player(current_player,
+                                position_for_player(current_player),
+                                current_category)
+
         ask_question
       end
     end
-    
+
   private
-    
+
     def ask_question
-      puts @pop_questions.shift if current_category == 'Pop'
-      puts @science_questions.shift if current_category == 'Science'
-      puts @sports_questions.shift if current_category == 'Sports'
-      puts @rock_questions.shift if current_category == 'Rock'
+      question = case current_category
+                 when 'Pop'
+                   @pop_questions.shift
+                 when 'Science'
+                   @science_questions.shift
+                 when 'Sports'
+                   @sports_questions.shift
+                 when 'Rock'
+                   @rock_questions.shift
+                 else
+                   raise UnknownCategoryError, current_category
+                 end
+
+      @moderator.announce_question(question)
     end
-    
+
     def current_category
       return 'Pop' if @places[@current_player_index] == 0
       return 'Pop' if @places[@current_player_index] == 4
@@ -121,63 +132,61 @@ module UglyTrivia
       return 'Sports' if @places[@current_player_index] == 10
       return 'Rock'
     end
-  
+
   public
-  
+
     def was_correctly_answered
       if in_penalty_box?(current_player)
         if @is_getting_out_of_penalty_box
-          puts 'Answer was correct!!!!'
+          @moderator.correct_answer(current_player,
+                                    @purses[@current_player_index])
+
           @purses[@current_player_index] += 1
-          puts @players[@current_player_index] +
-            ' now has ' +
-            @purses[@current_player_index] +
-            ' Gold Coins.'
-          
+
           winner = did_player_win()
           @current_player_index += 1
           @current_player_index = 0 if @current_player_index == @players.length
-          
+
           winner
         else
           @current_player_index += 1
           @current_player_index = 0 if @current_player_index == @players.length
           true
         end
-        
-        
-        
+
+
+
       else
-        
-        puts "Answer was corrent!!!!"
+
+        @moderator.correct_answer(
+          current_player,
+          @purses[@current_player_index]
+        )
+
         @purses[@current_player_index] += 1
-        puts @players[@current_player_index] +
-          ' now has ' +
-          @purses[@current_player_index].to_s +
-          ' Gold Coins.'
-        
+
         winner = did_player_win
         @current_player_index += 1
         @current_player_index = 0 if @current_player_index == @players.length
-        
+
         return winner
       end
     end
-    
+
     def wrong_answer
-  		puts 'Question was incorrectly answered'
-  		puts @players[@current_player_index] + " was sent to the penalty box"
+      @moderator.wrong_answer(current_player)
   		@in_penalty_box[@current_player_index] = true
 
       @current_player_index += 1
       @current_player_index = 0 if @current_player_index == @players.length
   		return true
     end
-    
+
   private
-  
+
     def did_player_win
       !(@purses[@current_player_index] == 6)
     end
   end
+
 end
---
+++ ruby/lib/ugly_trivia/moderators.rb
@@ -0,0 +1,57 @@
+require "stringio"
+require "forwardable"
+
+module UglyTrivia
+  class GameModerator
+    extend Forwardable
+    def_delegator :@out, :puts
+
+    def initialize(out=$stdout)
+      @out = out
+    end
+
+    def wrong_answer(player)
+  		puts 'Question was incorrectly answered'
+  		puts "#{player} was sent to the penalty box"
+    end
+
+    def correct_answer(player,coins)
+      puts 'Answer was correct!'
+      puts "#{player} now has #{coins} Gold coins."
+    end
+
+    def moved_player(player,location,category)
+      puts "#{player}'s new location is: #{location}"
+      puts "The category is #{category}"
+    end
+
+    def current_player(player)
+      puts "#{player} is the current player"
+    end
+
+    def added_player(player,number)
+      puts "#{player} was added as number #{number}."
+    end
+
+    def rolled(player,roll)
+      puts "#{player} got a...#{roll}!"
+    end
+
+    def player_gets_out_of_penalty_box(player)
+      puts "#{player} is getting out of the penalty box."
+    end
+
+    def player_stays_in_penalty_box(player)
+      puts "#{player} is not getting out of the penalty box."
+    end
+
+    def announce_question(category)
+      puts "Next question: '#{category}'"
+    end
+
+    def self.silent
+      new(StringIO.new)
+    end
+  end
+
+end
